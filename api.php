d26d416f7437cba49e5c36abe6358949285f9aaf
mertt52 <85098191+mertt52@users.noreply.github.com>
2021-11-07 22:27:12 +0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(0);
+
+$nick = $_GET['nick'];
+
+$url = 'https://instagram.com/' . $nick;
+    $html = file_get_contents($url);
+    $dom = new DOMDocument();
+    $dom->loadHTML($html);
+ 
+
+    $meta_tags = $dom->getElementsByTagName('meta');
+  foreach( $meta_tags as $meta ) {
+        if( $meta->getAttribute('property') == 'og:image' ) {
+            $pp= $meta->getAttribute('content');   }    }
+    foreach( $meta_tags as $meta ) {
+        if( $meta->getAttribute('property') == 'og:description' ) {
+            $sonveri= $meta->getAttribute('content');   }    }
+ $dizi = explode (" - ",$sonveri); 
+$dizi = explode (" ",$dizi[0]); 
+$m = $dizi[0];
+$mm = $dizi[2];
+$mmm= $dizi[4]; 
+
+
+echo $m;
+
+?>
